86620e318d61f6e8e8e7d35cb43c266935218d48
austinlavalley <hello@austinlavalley.com>
2024-07-18 10:22:51 -0500

    animating up settings view added

--- odo/ContentView.swift
+++ odo/ContentView.swift
@@ -8,63 +8,78 @@
 import SwiftUI
 
 struct ContentView: View {
-    
     @StateObject var hkManager = HealthKitManager.shared
+
+    @State private var showSettings = false
 
     var body: some View {
         NavigationStack {
             VStack {
-                Spacer()
-                
-                OdometerView(steps: hkManager.stepCountToday)
-                
-                Spacer()
-                
-                VStack {
-                    
-                    HStack {
-                        Text("Day Start - 24h period")
-                    }
-                    .frame(maxWidth: .infinity, maxHeight: 72)
-                    .font(.subheadline).fontDesign(.monospaced).bold()
-                    .foregroundStyle(.white)
-                    .background(.black)
-                    .clipShape(RoundedRectangle(cornerRadius: 12))
-                    .overlay { RoundedRectangle(cornerRadius: 12).stroke(.white, lineWidth: 2) }
-                    
-                    HStack {
-                        Text("Day Start - 24h period")
-                    }
-                    .frame(maxWidth: .infinity, maxHeight: 72)
-                    .font(.subheadline).fontDesign(.monospaced).bold()
-                    .foregroundStyle(.white)
-                    .background(.black)
-                    .clipShape(RoundedRectangle(cornerRadius: 12))
-                    .overlay { RoundedRectangle(cornerRadius: 12).stroke(.white, lineWidth: 2) }
-                    
-                    HStack {
-                        Text("Day Start - 24h period")
-                    }
-                    .frame(maxWidth: .infinity, maxHeight: 72)
-                    .font(.subheadline).fontDesign(.monospaced).bold()
-                    .foregroundStyle(.white)
-                    .background(.black)
-                    .clipShape(RoundedRectangle(cornerRadius: 12))
-                    .overlay { RoundedRectangle(cornerRadius: 12).stroke(.white, lineWidth: 2) }
-                    
-                    
-                }.padding()
+                if showSettings {
+                    SettingsView()
+                        .transition(.move(edge: .bottom))
+                } else {
+                    MainView()
+                        .transition(.move(edge: .top))
+                }
             }
             .toolbar {
                 ToolbarItem(placement: .primaryAction) {
-                    Button {} label: {
-                        Image(systemName: "gear").foregroundStyle(.white)
+                    Button(action: {
+                        withAnimation {
+                            showSettings.toggle()
+                        }
+                    }) {
+                        Image(systemName: "gear")
+                            .foregroundColor(.white)
                     }
                 }
             }
         }
     }
 }
+
+
+struct MainView: View {
+    @StateObject var hkManager = HealthKitManager.shared
+    
+    var body: some View {
+        Spacer()
+        
+        OdometerView(steps: hkManager.stepCountToday)
+        
+        Spacer()
+        
+        VStack {
+            
+
+            
+            
+        }.padding()
+    }
+}
+
+
+struct SettingsView: View {
+    var body: some View {
+        VStack {
+            HStack {
+                Text("Day Start - 24h period")
+            }
+            .frame(maxWidth: .infinity, maxHeight: 72)
+            .font(.subheadline).fontDesign(.monospaced).bold()
+            .foregroundStyle(.white)
+            .background(.black)
+            .clipShape(RoundedRectangle(cornerRadius: 12))
+            .overlay { RoundedRectangle(cornerRadius: 12).stroke(.white, lineWidth: 2) }
+        }
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .background(.black)
+            .foregroundColor(.white)
+            .font(.title)
+    }
+}
+
 
 #Preview {
     ContentView()
--- odo/odoApp.swift
+++ odo/odoApp.swift
@@ -12,6 +12,7 @@ struct odoApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .preferredColorScheme(.dark)
         }
     }
 }
